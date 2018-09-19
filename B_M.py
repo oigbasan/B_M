73add2ce1dd8863bfa7536956d6fc6b87b5a1f52
oigbasan <oigbasan@uwo.ca>
2018-09-19 18:43:35 -0400

    initial commit

---
+++ B_M.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 18:12:26 2018
+
+@author: owner
+"""
+
+print("bread products")
+print("list of bread products")
---
+++ readme.txt
@@ -0,0 +1,1 @@
+my test for branching and merging
